1c1f192d95548c6bfbdd7e528557b0a27039e621
Phil Rosenfield <philrosenfield@gmail.com>
2013-05-31 12:20:16 -0500

    isotrack file reader ...
    might not need it actually, might need to adjust tracks.

--- TrilegalUtils.py
+++ TrilegalUtils.py
@@ -591,6 +591,51 @@ def string_in_lines(lines):
                 print line, strs
 
 
+class isotrack2(object):
+    def __init__(self, filename):
+        self.read_ptcri2(filename)
+        self.base, self.name = os.path.split(filename)
+
+    def read_ptcri2(self, filename):
+        '''
+        header fmt:
+        2 # number of isochrones sets with same eeps. in this case 2 (could be has 1st TP vs only C_BUR)
+        1 18 # which isochrones have first eep set
+        19 26 # which isochrones have the second eep set
+        26 # number of isochrones
+        384 398 395 397 392 ... number of age steps in each isochrone
+        1.750000 1.800000 1.850000 ... masses of each isochrone
+        '''
+        lines = [l.strip() for l in open(filename, 'r').readlines()]
+        # file made to be read by C, going line by line for header info.
+        eep_sets = int(lines[0])
+        set_inds = np.array([map(int, lines[i + 1].split()) for i in range(eep_sets)])
+        set_inds[:, 0] -= 1 
+        nsets = int(lines[eep_sets + 1])
+        len_tracks = np.array(lines[eep_sets + 2].split(), dtype=int)
+        masses = np.array(lines[eep_sets + 3].split(), dtype=float)
+        start_ind = eep_sets + 4
+
+        start_inds = np.cumsum(len_tracks) + start_ind
+        start_inds = np.insert(start_inds, 0, start_ind)
+        end_inds = start_inds - 1
+
+        # each one is too long
+        end_inds = end_inds[1:]
+        start_inds = start_inds[:-1]
+
+        self.eep_sets = eep_sets
+        self.nsets = nsets
+        self.masses = masses
+        for i in range(eep_sets):
+            set_start, set_end = set_inds[i]
+            isoc_strs = [lines[start_inds[j]: end_inds[j]] for j in arange(*set_inds[i])]
+            isoc_data = np.array([np.array([isoc_strs[j][k].split()[:3] for k in range(len(isoc_strs[j]))], dtype=float) for j in range(len(isoc_strs))])
+            isoc_masses = ['%.4f' % m for m in masses[np.arange(*set_inds[i])]]
+            self.__setattr__('iso_str%i' % (i + 1), {isoc_masses[j]: isoc_strs[j] for j in range(len(isoc_masses))})
+            self.__setattr__('iso_data%i' % (i + 1), {isoc_masses[j]: isoc_data[j] for j in range(len(isoc_masses))})
+# get the EEPS too?? I donno what you need the data for besides plotting...
+
 def read_ptcri(ptcri_file):
     d = {}
     lines = open(ptcri_file, 'r').readlines()
